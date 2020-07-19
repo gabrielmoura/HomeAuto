c850c4e136bd370f803ac080bc5b46659c05f471
Gabriel Moura <develop@srmoura.com.br>
2020-07-19 04:20:26 -0300

    1.0.2

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,196 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+if (ons.platform.isIPhoneX()) { // Utility function
+    // Add empty attribute to the <html> element
+    document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+  }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+  if (ons.platform.isIPhoneX()) { // Utility function
+    // Add empty attribute to the <html> element
+    document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+  }
+  // Setup Button Handler
+  document.addEventListener('init', function(event) {
+    var page = event.target;
+    if (page.id === 'setup') {
+      
+      page.querySelector('#go-to-main').onclick = function() {
+        // Store in localStorage
+        //localStorage.setItem("ip",document.getElementById('ip').value);
+        //localStorage.setItem("sub",document.getElementById('sub').value);
+        // no secret key needed localStorage.setItem("sec",document.getElementById('sec').value);
+        setIP(document.getElementById('ip').value);
+        getFunc();
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
+      };
     }
+    else if (page.id === 'setup-return') {
+      page.querySelector('#go-to-main').onclick = function() {
+        // Store in localStorage
+        //localStorage.setItem("ip",document.getElementById('ip').value);
+        //localStorage.setItem("sub",document.getElementById('sub').value);
+        setIP(document.getElementById('ip').value);
+        getFunc();
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
+      };
+      page.querySelector('#go-cancel').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
+      };
+    }
+    else if (page.id === 'index') {
+    
+      if (localStorage.getItem("ip") === null) {
+        //document.querySelector('#myNavigator').pushPage('setup.html', {data: {title: 'Auto'}});
+        document.querySelector('#myNavigator').pushPage('setup.html', {data: {title: 'Setup'}});
+      }else{
+        getFunc(); 
+      }
+      
+    } 
+    
+  });
+        
+  
+  function setIP(ip){
+    localStorage.setItem("ip","http://"+ip)
+    ons.notification.toast("IP definido",{timeout:15000})
+  }
+  function getFunc(){
+    axios.get(localStorage.getItem("ip")+'/gpio/list')
+      .then(function (response) {
+        
+        for (i in response.data) {
+           if(response.data[i].pin == 4){
+              if(response.data[i].status == 0){
+               
+              localStorage.setItem("cozinha", "off");
+              document.getElementById('cozinha').checked=false
+              
+             }else{
+              localStorage.setItem("cozinha", "on");       
+              document.getElementById('cozinha').checked=true
+             }
+           
+           }       
+           if(response.data[i].pin == 17){
+            if(response.data[i].status == 0){
+             
+            localStorage.setItem("sala", "off");
+            document.getElementById('sala').checked=false
+            
+           }else{
+            localStorage.setItem("sala", "on");       
+            document.getElementById('sala').checked=true
+           }
+         
+         }       
+        }
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao acessar dispositivo',{timeout: 1500});
+      })
+  }
+  function onkitchen(){
+    if(localStorage.getItem("cozinha")=="off"){
+      axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin0:4,
+          val0:1
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("cozinha", "off");
+        getFunc();
+        ons.notification.toast('Cozinha desligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao desligar Cozinha',{timeout: 1500});
+      })
+    }
+    if(localStorage.getItem("cozinha")=="on"){
+    axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin0:4,
+          val0:0
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("cozinha", "on");
+        getFunc();
+        ons.notification.toast('Cozinha ligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        
+        ons.notification.toast(error,{timeout: 1500});
+      })
+    }
+  }
+  function onliving(){
+    if(localStorage.getItem("sala")=="off"){
+      axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin1:17,
+          val1:1
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("sala", "off");
+        getFunc();
+        ons.notification.toast('sala desligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao desligar sala',{timeout: 1500});
+      })
+    }
+    if(localStorage.getItem("sala")=="on"){
+    axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin1:17,
+          val1:0
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("sala", "on");
+        getFunc();
+        ons.notification.toast('sala ligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        
+        ons.notification.toast(error,{timeout: 1500});
+      })
+    }
+  }
+  
+  
+
+
+window.fn = {};
+
+window.fn.open = function() {
+var menu = document.getElementById('menu');
+menu.open();
 };
 
-app.initialize();
+window.fn.load = function(page, data) {
+var content = document.getElementById('myNavigator');
+var menu = document.getElementById('menu');
+content.pushPage(page, data)
+.then(menu.close.bind(menu));
+
+//getFunc();
+};
+
+window.fn.pop = function() {
+var content = document.getElementById('myNavigator');
+content.popPage();
+};
--- www/js/index.js
+++ www/js/index.js
@@ -13,50 +13,106 @@ if (ons.platform.isIPhoneX()) { // Utility function
   document.addEventListener('init', function(event) {
     var page = event.target;
     if (page.id === 'setup') {
+      
       page.querySelector('#go-to-main').onclick = function() {
         // Store in localStorage
-        localStorage.setItem("pub",document.getElementById('pub').value);
-        localStorage.setItem("sub",document.getElementById('sub').value);
+        //localStorage.setItem("ip",document.getElementById('ip').value);
+        //localStorage.setItem("sub",document.getElementById('sub').value);
         // no secret key needed localStorage.setItem("sec",document.getElementById('sec').value);
-        document.querySelector('#myNavigator').pushPage('feed.html', {data: {title: 'Auto'}});
+        setIP(document.getElementById('ip').value);
+        getFunc();
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
       };
     }
     else if (page.id === 'setup-return') {
       page.querySelector('#go-to-main').onclick = function() {
         // Store in localStorage
-        localStorage.setItem("pub",document.getElementById('pub').value);
-        localStorage.setItem("sub",document.getElementById('sub').value);
-
-        document.querySelector('#myNavigator').pushPage('feed.html', {data: {title: 'Auto'}});
+        //localStorage.setItem("ip",document.getElementById('ip').value);
+        //localStorage.setItem("sub",document.getElementById('sub').value);
+        setIP(document.getElementById('ip').value);
+        getFunc();
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
       };
       page.querySelector('#go-cancel').onclick = function() {
-        document.querySelector('#myNavigator').pushPage('feed.html', {data: {title: 'Auto'}});
+        document.querySelector('#myNavigator').pushPage('index.html', {data: {title: 'Auto'}});
       };
     }
-    else if (page.id === 'feed') {
-      page.querySelector('#enter-setup').onclick = function() {
-        document.querySelector('#myNavigator').pushPage('setup-return.html', {data: {title: 'Auto'}});
-      };
-    } 
-  });
-  var shouldIGoToSetup = false;
-  // end setup button handler
-  if (localStorage.getItem("sub") === null || localStorage.getItem("pub") === null) {
-    // hi
-    shouldIGoToSetup = true;
-    // if this doesn't work, make it set a variable, and use a script tag in feed to launch the page
-  }
-  else {
-    // Do some init stuff
-  }      
-  function toastThing() {
+    else if (page.id === 'index') {
     
-    //myToast.toggle();
-    ons.notification.toast('Toast',{timeout: 2000});
-  }
+      if (localStorage.getItem("ip") === null) {
+        //document.querySelector('#myNavigator').pushPage('setup.html', {data: {title: 'Auto'}});
+        document.querySelector('#myNavigator').pushPage('setup.html', {data: {title: 'Setup'}});
+      }else{
+        getFunc(); 
+      }
+      
+    } 
+    
+  });
+        
   
+  function setIP(ip){
+    localStorage.setItem("ip","http://"+ip)
+    ons.notification.toast("IP definido",{timeout:15000})
+  }
+  function getFunc(){
+    axios.get(localStorage.getItem("ip")+'/gpio/list')
+      .then(function (response) {
+        
+        for (i in response.data) {
+           if(response.data[i].pin == 4){
+              if(response.data[i].status == 0){
+               
+              localStorage.setItem("cozinha", "off");
+              document.getElementById('cozinha').checked=false
+              
+             }else{
+              localStorage.setItem("cozinha", "on");       
+              document.getElementById('cozinha').checked=true
+             }
+           
+           }       
+           if(response.data[i].pin == 17){
+            if(response.data[i].status == 0){
+             
+            localStorage.setItem("sala", "off");
+            document.getElementById('sala').checked=false
+            
+           }else{
+            localStorage.setItem("sala", "on");       
+            document.getElementById('sala').checked=true
+           }
+         
+         }       
+        }
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao acessar dispositivo',{timeout: 1500});
+      })
+  }
   function onkitchen(){
-    axios.get('http://192.168.1.116:8010/gpio/set', {
+    if(localStorage.getItem("cozinha")=="off"){
+      axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin0:4,
+          val0:1
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("cozinha", "off");
+        getFunc();
+        ons.notification.toast('Cozinha desligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao desligar Cozinha',{timeout: 1500});
+      })
+    }
+    if(localStorage.getItem("cozinha")=="on"){
+    axios.get(localStorage.getItem("ip")+'/gpio/set', {
         params: {
           pin0:4,
           val0:0
@@ -64,18 +120,59 @@ if (ons.platform.isIPhoneX()) { // Utility function
       })
       .then(function (response) {
         console.log(response);
-        ons.notification.toast('Sala ligada',{timeout: 2000});
+        //localStorage.setItem("cozinha", "on");
+        getFunc();
+        ons.notification.toast('Cozinha ligada',{timeout: 1500});
       })
       .catch(function (error) {
         console.log(error);
+        
+        ons.notification.toast(error,{timeout: 1500});
       })
-  } 
-  
-  function ifgoToSetup() {
-    if (shouldIGoToSetup) {
-      document.querySelector('#myNavigator').pushPage('setup.html', {data: {title: 'Name'}});
     }
   }
+  function onliving(){
+    if(localStorage.getItem("sala")=="off"){
+      axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin1:17,
+          val1:1
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("sala", "off");
+        getFunc();
+        ons.notification.toast('sala desligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        ons.notification.toast('Erro ao desligar sala',{timeout: 1500});
+      })
+    }
+    if(localStorage.getItem("sala")=="on"){
+    axios.get(localStorage.getItem("ip")+'/gpio/set', {
+        params: {
+          pin1:17,
+          val1:0
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+        //localStorage.setItem("sala", "on");
+        getFunc();
+        ons.notification.toast('sala ligada',{timeout: 1500});
+      })
+      .catch(function (error) {
+        console.log(error);
+        
+        ons.notification.toast(error,{timeout: 1500});
+      })
+    }
+  }
+  
+  
+
 
 window.fn = {};
 
@@ -89,6 +186,8 @@ var content = document.getElementById('myNavigator');
 var menu = document.getElementById('menu');
 content.pushPage(page, data)
 .then(menu.close.bind(menu));
+
+//getFunc();
 };
 
 window.fn.pop = function() {
